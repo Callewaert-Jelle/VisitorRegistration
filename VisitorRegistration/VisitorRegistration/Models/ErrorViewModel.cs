aedef9ba548752c720480a8a8ca5c8967f519105
Jelle Callewaert <jelle.callewaert@hotmail.com>
2020-09-20 18:51:50 +0200

    Created main project

---
+++ VisitorRegistration/VisitorRegistration/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VisitorRegistration.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
