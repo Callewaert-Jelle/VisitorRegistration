b85a0762efe78867d84536efd3df9ddd86772249
Jelle Callewaert <jelle.callewaert@hotmail.com>
2020-09-21 13:19:48 +0200

    Added Visitation controller & views
    Controller with views for
    - Register
    - Log out

---
+++ VisitorRegistration/VisitorRegistration/Controllers/VisitationController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VisitorRegistration.Controllers
+{
+    public class VisitationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult LogOut()
+        {
+            return View();
+        }
+    }
+}
